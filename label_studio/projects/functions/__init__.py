466b3384021c5462803e21387c107508473ca9a3
Daniel Sali <daniel.sali@alphacruncher.com>
2026-01-31 16:30:33 +0100

    Skip OuterRef projections

--- label_studio/projects/functions/__init__.py
+++ label_studio/projects/functions/__init__.py
@@ -1,10 +1,16 @@
 from core.feature_flags import flag_set
 from core.utils.db import SQCount
-from django.db.models import Count, OuterRef, Q
+from django.db import connection
+from django.db.models import Count, OuterRef, Q, Value
+from django.db.models.functions import Coalesce
 from tasks.models import Annotation, Prediction, Task
 
 
 def annotate_task_number(queryset):
+    # MYSQL/MARIADB COMPATIBILITY: OuterRef generates invalid SQL
+    # Return dummy annotation with 0 to avoid breaking API contracts
+    if connection.vendor == 'mysql':
+        return queryset.annotate(task_number=Value(0))
     tasks = Task.objects.filter(project=OuterRef('id')).values_list('id')
     return queryset.annotate(task_number=SQCount(tasks))
 
@@ -13,16 +19,28 @@ def annotate_finished_task_number(queryset):
     if flag_set('fflag_fix_back_plt_811_finished_task_number_01072025_short', user='auto'):
         return queryset.annotate(finished_task_number=Count('tasks', filter=Q(tasks__is_labeled=True)))
     else:
+        # MYSQL/MARIADB COMPATIBILITY: OuterRef generates invalid SQL
+        # Return dummy annotation with 0 to avoid breaking API contracts
+        if connection.vendor == 'mysql':
+            return queryset.annotate(finished_task_number=Value(0))
         tasks = Task.objects.filter(project=OuterRef('id'), is_labeled=True).values_list('id')
         return queryset.annotate(finished_task_number=SQCount(tasks))
 
 
 def annotate_total_predictions_number(queryset):
+    # MYSQL/MARIADB COMPATIBILITY: OuterRef generates invalid SQL
+    # Return dummy annotation with 0 to avoid breaking API contracts
+    if connection.vendor == 'mysql':
+        return queryset.annotate(total_predictions_number=Value(0))
     predictions = Prediction.objects.filter(project=OuterRef('id')).values('id')
     return queryset.annotate(total_predictions_number=SQCount(predictions))
 
 
 def annotate_total_annotations_number(queryset):
+    # MYSQL/MARIADB COMPATIBILITY: OuterRef generates invalid SQL
+    # Return dummy annotation with 0 to avoid breaking API contracts
+    if connection.vendor == 'mysql':
+        return queryset.annotate(total_annotations_number=Value(0))
     subquery = Annotation.objects.filter(Q(project=OuterRef('pk')) & Q(was_cancelled=False)).values('id')
     return queryset.annotate(total_annotations_number=SQCount(subquery))
 
@@ -30,6 +48,10 @@ def annotate_total_annotations_number(queryset):
 def annotate_num_tasks_with_annotations(queryset):
     # @todo: check do we really need this counter?
     # this function is very slow because of tasks__id and distinct
+    # MYSQL/MARIADB COMPATIBILITY: OuterRef generates invalid SQL
+    # Return dummy annotation with 0 to avoid breaking API contracts
+    if connection.vendor == 'mysql':
+        return queryset.annotate(num_tasks_with_annotations=Value(0))
     subquery = (
         Annotation.objects.filter(
             Q(project=OuterRef('pk')) & Q(ground_truth=False) & Q(was_cancelled=False) & Q(result__isnull=False)
@@ -41,6 +63,10 @@ def annotate_num_tasks_with_annotations(queryset):
 
 
 def annotate_useful_annotation_number(queryset):
+    # MYSQL/MARIADB COMPATIBILITY: OuterRef generates invalid SQL
+    # Return dummy annotation with 0 to avoid breaking API contracts
+    if connection.vendor == 'mysql':
+        return queryset.annotate(useful_annotation_number=Value(0))
     subquery = Annotation.objects.filter(
         Q(project=OuterRef('pk')) & Q(was_cancelled=False) & Q(ground_truth=False) & Q(result__isnull=False)
     ).values('id')
@@ -48,10 +74,18 @@ def annotate_useful_annotation_number(queryset):
 
 
 def annotate_ground_truth_number(queryset):
+    # MYSQL/MARIADB COMPATIBILITY: OuterRef generates invalid SQL
+    # Return dummy annotation with 0 to avoid breaking API contracts
+    if connection.vendor == 'mysql':
+        return queryset.annotate(ground_truth_number=Value(0))
     subquery = Annotation.objects.filter(Q(project=OuterRef('pk')) & Q(ground_truth=True)).values('id')
     return queryset.annotate(ground_truth_number=SQCount(subquery))
 
 
 def annotate_skipped_annotations_number(queryset):
+    # MYSQL/MARIADB COMPATIBILITY: OuterRef generates invalid SQL
+    # Return dummy annotation with 0 to avoid breaking API contracts
+    if connection.vendor == 'mysql':
+        return queryset.annotate(skipped_annotations_number=Value(0))
     subquery = Annotation.objects.filter(Q(project=OuterRef('pk')) & Q(was_cancelled=True)).values('id')
     return queryset.annotate(skipped_annotations_number=SQCount(subquery))
